1ade4cf5b5b963c21df98b590fdf92ca53e95f48
Nathaniel Klump <nathanieljklump@gmail.com>
2023-07-28 00:51:29 -0400

    add day cycle

--- server/server.js
+++ server/server.js
@@ -1,27 +1,40 @@
-import express from 'express';
-import fetch from 'node-fetch';
+import express from "express";
+import fetch from "node-fetch";
+import moment from "moment";
+
 const app = express();
 const port = 3001;
 
-const urls = ['https://background-music.onrender.com/status', 'https://herald-ppvi.onrender.com/status'];
+const urls = [
+  "https://herald-of-the-obese.onrender.com/status",
+];
 
-app.get('/', (req, res) => {
-    res.send('kept alive');
+app.get("/", (req, res) => {
+  res.send("kept alive");
 });
 
-
 const rooster = (urls) => {
-    for(let URL of urls) {
-        fetch(URL)
-        .then(res => res.text())
-        .then(resText => console.log(`received update: "${resText}" from: "${URL}"`))
-        .catch(error => console.error(`Error fetching ${URL}: `, error));
+  let date = moment().format("HHmm");
+  let integerDate = parseInt(date, 10);
+  if (
+    (integerDate >= 930 && integerDate <= 2400) ||
+    (integerDate >= 0 && integerDate < 100)
+  ) {
+    for (let URL of urls) {
+        console.log(`sent waker to ${URL}`)
+      fetch(URL)
+        .then((res) => res.text())
+        .then((resText) =>
+          console.log(`received update: "${resText}" from: "${URL}"`),
+        )
+        .catch((error) => console.error(`Error fetching ${URL}: `, error));
     }
-}
+  }
+};
 
 rooster(urls);
 setInterval(() => rooster(urls), 870000);
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`)
-})
+  console.log(`server started on port ${port}`);
+});
